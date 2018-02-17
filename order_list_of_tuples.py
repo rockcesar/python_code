24ea5d25221dedcaff15768365cf56e41f2df737
César Cordero <cesar.cordero.r@gmail.com>
2018-02-17 10:00:56 -0400

    Order list or tuples with sorted.

---
+++ order_list_of_tuples.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+    Programa desarrollado por el Licenciado César Cordero Rodríguez.
+    Bajo licencia GPLv3.
+    Puede ser modificado, copiado y distribuido.
+    Se desarrolló con fines educativos.
+
+"""
+
+sorted({'1':'hi','2':'hello','3':'bye'}.items(), key=lambda x: x[0])
