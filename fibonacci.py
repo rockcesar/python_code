edad54f7f7ef49a73e3887a6fbb1ca1c55336af7
rockcesar <rockcesar@gmail.com>
2019-05-27 20:03:30 -0400

    fibonacci.py
    fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+    Programa desarrollado por el Licenciado César Cordero Rodríguez.
+    Bajo licencia GPLv3.
+    Puede ser modificado, copiado y distribuido.
+    Se desarrolló con fines educativos.
+    
+    Si se hacen copias, debe ser referido el autor: Licenciado César Cordero Rodríguez.
+"""
+
 def fibonacci(number_1, number_2, first, count):
     print(str(number_1))
     if first < count:
@@ -11,9 +22,7 @@ def list_fibonacci(number_1, number_2, first, count, list_f):
     if first < count:
         first+=1
         return list_fibonacci(number_2, number_1 + number_2, first, count, list_f)
-
     return list_f
 
 list_f = list_fibonacci(1, 1, 1, 100, list())
-
 print(str(list_f))
