e6e3d7a828c0f1e8805d234bf34b33f7483f0699
rockcesar <rockcesar@gmail.com>
2019-06-05 16:45:41 -0400

    A translator in Python using Tor Proxy
    A translator in Python using Tor Proxy

---
+++ translate.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""
+    Programa desarrollado por el Licenciado César Cordero Rodríguez.
+    Bajo licencia GPLv3.
+    Puede ser modificado, copiado y distribuido.
+    Se desarrolló con fines educativos.
+    
+    Si se hacen copias, debe ser referido el autor: Licenciado César Cordero Rodríguez.
+"""
+
+A translator in Python using Tor Proxy:
+
+from py_translator import Translator
+import sys
+
+proxy = {
+        'socks': 'socks://localhost:9050',
+        'socks': 'socks://localhost:9050',
+}
+s = Translator(proxies=proxy).translate(text=sys.argv[1], dest='es').text
+print(s)
