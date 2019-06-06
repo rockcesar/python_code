1c4548e773461a790d5904cbbc09ab52b62b690f
rockcesar <rockcesar@gmail.com>
2019-06-06 07:57:28 -0400

    Update translate.py

--- translate.py
+++ translate.py
@@ -14,9 +14,25 @@
 from py_translator import Translator
 import sys
 
+#from py_translator import TEXTLIB
+
 proxy = {
         'socks': 'socks://localhost:9050',
         'socks': 'socks://localhost:9050',
 }
-s = Translator(proxies=proxy).translate(text=sys.argv[1], dest=sys.argv[2]).text
+
+ROTATING_PROXY_LIST = {
+    'socks': 'socks://localhost:9050'
+}
+
+text = ""
+
+if len(sys.argv) == 3:
+   text = sys.argv[1]
+elif len(sys.argv) == 4 and sys.argv[3] == "using_file":
+   file = open("file_to_translate.txt", "r")
+   text = file.read()
+
+s = Translator(proxies=proxy).translate(text=text, dest=sys.argv[2]).text
+
 print(s)
