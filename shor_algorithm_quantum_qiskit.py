2f3c0a95e87b5fb1f1af89ac4b01e23053b896c0
César Cordero Rodríguez <cesar.cordero.r@gmail.com>
2023-07-18 19:28:35 -0400

    Add files via upload

---
+++ shor_algorithm_quantum_qiskit.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+
+"""
+    Programa desarrollado por el Licenciado César Cordero Rodríguez.
+    Bajo licencia GPLv3.
+    Puede ser modificado, copiado y distribuido.
+    Se desarrolló con fines educativos.
+"""
+
+#Read more here: https://learn.qiskit.org/course/ch-algorithms/shors-algorithm
+
+#TO INSTALL:
+#pip3 install qiskit
+#pip3 install matplotlib
+#pip3 install pylatexenc
+#pip3 install numpy matplotlib pandas
+
+import matplotlib.pyplot as plt
+import numpy as np
+from qiskit import QuantumCircuit, BasicAer, Aer, transpile
+from qiskit.visualization import plot_histogram
+from math import gcd
+from numpy.random import randint
+import pandas as pd
+from fractions import Fraction
+print("Imports Successful")
+
+def c_amod15(a, power):
+    """Controlled multiplication by a mod 15"""
+    if a not in [2,4,7,8,11,13]:
+        raise ValueError("'a' must be 2,4,7,8,11 or 13")
+    U = QuantumCircuit(4)
+    for _iteration in range(power):
+        if a in [2,13]:
+            U.swap(2,3)
+            U.swap(1,2)
+            U.swap(0,1)
+        if a in [7,8]:
+            U.swap(0,1)
+            U.swap(1,2)
+            U.swap(2,3)
+        if a in [4, 11]:
+            U.swap(1,3)
+            U.swap(0,2)
+        if a in [7,11,13]:
+            for q in range(4):
+                U.x(q)
+    U = U.to_gate()
+    U.name = f"{a}^{power} mod 15"
+    c_U = U.control()
+    return c_U
+
+def qft_dagger(n):
+    """n-qubit QFTdagger the first n qubits in circ"""
+    qc = QuantumCircuit(n)
+    # Don't forget the Swaps!
+    for qubit in range(n//2):
+        qc.swap(qubit, n-qubit-1)
+    for j in range(n):
+        for m in range(j):
+            qc.cp(-np.pi/float(2**(j-m)), m, j)
+        qc.h(j)
+    qc.name = "QFT†"
+    return qc
+
+# Specify variables
+N_COUNT = 8  # number of counting qubits
+a = 7
+
+# Create QuantumCircuit with N_COUNT counting qubits
+# plus 4 qubits for U to act on
+qc = QuantumCircuit(N_COUNT + 4, N_COUNT)
+
+# Initialize counting qubits
+# in state |+>
+for q in range(N_COUNT):
+    qc.h(q)
+
+# And auxiliary register in state |1>
+qc.x(N_COUNT)
+
+# Do controlled-U operations
+for q in range(N_COUNT):
+    qc.append(c_amod15(a, 2**q),
+             [q] + [i+N_COUNT for i in range(4)])
+
+# Do inverse-QFT
+qc.append(qft_dagger(N_COUNT), range(N_COUNT))
+
+# Measure circuit
+qc.measure(range(N_COUNT), range(N_COUNT))
+qc.draw(fold=-1)  # -1 means 'do not fold'
+
+aer_sim = Aer.get_backend('aer_simulator')
+t_qc = transpile(qc, aer_sim)
+counts = aer_sim.run(t_qc).result().get_counts()
+plot_histogram(counts)
+
+rows, measured_phases = [], []
+for output in counts:
+    decimal = int(output, 2)  # Convert (base 2) string to decimal
+    phase = decimal/(2**N_COUNT)  # Find corresponding eigenvalue
+    measured_phases.append(phase)
+    # Add these values to the rows in our table:
+    rows.append([f"{output}(bin) = {decimal:>3}(dec)",
+                 f"{decimal}/{2**N_COUNT} = {phase:.2f}"])
+# Print the rows in a table
+headers=["Register Output", "Phase"]
+df = pd.DataFrame(rows, columns=headers)
+print(df)
+
+plt.show()
